6cafa95e06abb910c3ba233fc81b5877a4d27595
Matt Whiteley <mwhiteley@engineyard.com>
2013-06-19 22:52:18 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# cookbook-test cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Matt Whiteley (<mattwhiteley@gmail.com>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'cookbook-test'
+maintainer       'Matt Whiteley'
+maintainer_email 'mattwhiteley@gmail.com'
+license          'Apache 2.0'
+description      'Installs/Configures cookbook-test'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
